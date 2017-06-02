37170c4b9ce7f57d70d4a3353da1786be7bd1ad0
Dmitro Bondarenko <bondes87@gmail.com>
2017-06-02 23:20:39 +0300

    Counted the frequency of bytes in the file.

--- src/com/shpp/dbondarenko/Archiver.java
+++ src/com/shpp/dbondarenko/Archiver.java
@@ -1,8 +1,10 @@
 package com.shpp.dbondarenko;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
 
 /**
  * File: com.shpp.dbondarenko.Archiver.java
@@ -11,7 +13,32 @@ import java.io.IOException;
 public class Archiver {
     public void createArchive(String fileName) throws IOException {
         byte[] bytesFromFile = readFileToBytes(fileName);
+        fileArchiving(bytesFromFile);
         System.out.println(bytesFromFile.length);
+    }
+
+    private void fileArchiving(byte[] bytesFromFile) {
+        HashMap<Byte, Integer> countBytesFrequencyOfFile = countBytesFrequency(bytesFromFile);
+        int count=0;
+        for (Map.Entry<Byte, Integer> entry : countBytesFrequencyOfFile.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+            count+=entry.getValue();
+        }
+        System.out.println(countBytesFrequencyOfFile.size());
+        System.out.println(count);
+
+    }
+
+    private HashMap<Byte, Integer> countBytesFrequency(byte[] bytesFromFile) {
+        HashMap<Byte, Integer> countBytesFrequencyOfFile = new HashMap<>();
+        for (byte oneByte : bytesFromFile) {
+            if (countBytesFrequencyOfFile.containsKey(oneByte)) {
+                countBytesFrequencyOfFile.put(oneByte, countBytesFrequencyOfFile.get(oneByte) + 1);
+            } else {
+                countBytesFrequencyOfFile.put(oneByte, 1);
+            }
+        }
+        return countBytesFrequencyOfFile;
     }
 
     private byte[] readFileToBytes(String fileName) throws IOException {
