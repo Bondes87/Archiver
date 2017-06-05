968bc0aef86ca355bbab74b8a2f6c9810843dd96
Dmitro Bondarenko <bondes87@gmail.com>
2017-06-05 15:14:42 +0300

    Sorted by the frequency values of bits.

--- src/com/shpp/dbondarenko/Archiver.java
+++ src/com/shpp/dbondarenko/Archiver.java
@@ -2,9 +2,7 @@ package com.shpp.dbondarenko;
 
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.*;
 
 /**
  * File: com.shpp.dbondarenko.Archiver.java
@@ -18,27 +16,46 @@ public class Archiver {
     }
 
     private void fileArchiving(byte[] bytesFromFile) {
-        HashMap<Byte, Integer> countBytesFrequencyOfFile = countBytesFrequency(bytesFromFile);
-        int count=0;
-        for (Map.Entry<Byte, Integer> entry : countBytesFrequencyOfFile.entrySet()) {
+        HashMap<Byte, Integer> bytesFrequencyOfFile = countBytesFrequency(bytesFromFile);
+        int count = 0;
+        for (Map.Entry<Byte, Integer> entry : bytesFrequencyOfFile.entrySet()) {
             System.out.println(entry.getKey() + ": " + entry.getValue());
-            count+=entry.getValue();
+            count += entry.getValue();
         }
-        System.out.println(countBytesFrequencyOfFile.size());
-        System.out.println(count);
-
+       /* System.out.println(bytesFrequencyOfFile.size());
+        System.out.println(count);*/
+        HashMap<Byte, Integer> sortedBytesFrequency = sortByValue(bytesFrequencyOfFile);
+        for (Map.Entry<Byte, Integer> entry : sortedBytesFrequency.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
     }
 
     private HashMap<Byte, Integer> countBytesFrequency(byte[] bytesFromFile) {
-        HashMap<Byte, Integer> countBytesFrequencyOfFile = new HashMap<>();
+        HashMap<Byte, Integer> bytesFrequencyOfFile = new HashMap<>();
         for (byte oneByte : bytesFromFile) {
-            if (countBytesFrequencyOfFile.containsKey(oneByte)) {
-                countBytesFrequencyOfFile.put(oneByte, countBytesFrequencyOfFile.get(oneByte) + 1);
+            if (bytesFrequencyOfFile.containsKey(oneByte)) {
+                bytesFrequencyOfFile.put(oneByte, bytesFrequencyOfFile.get(oneByte) + 1);
             } else {
-                countBytesFrequencyOfFile.put(oneByte, 1);
+                bytesFrequencyOfFile.put(oneByte, 1);
             }
         }
-        return countBytesFrequencyOfFile;
+        return bytesFrequencyOfFile;
+    }
+
+    private HashMap<Byte, Integer> sortByValue(HashMap<Byte, Integer> bytesFrequencyOfFile) {
+        HashMap<Byte, Integer> sortedBytesFrequency = new LinkedHashMap<>();
+        List<Map.Entry<Byte, Integer>> list = new ArrayList<>(bytesFrequencyOfFile.entrySet());
+        list.sort(new Comparator<Map.Entry<Byte, Integer>>() {
+            @Override
+            public int compare(Map.Entry<Byte, Integer> o1,
+                               Map.Entry<Byte, Integer> o2) {
+                return (o1.getValue()).compareTo(o2.getValue());
+            }
+        });
+        for (Map.Entry<Byte, Integer> entry : list) {
+            sortedBytesFrequency.put(entry.getKey(), entry.getValue());
+        }
+        return sortedBytesFrequency;
     }
 
     private byte[] readFileToBytes(String fileName) throws IOException {
