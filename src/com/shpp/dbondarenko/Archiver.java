af418a2c77358683831ed680fc3a9625577b366a
Dmitro Bondarenko <bondes87@gmail.com>
2017-06-06 13:59:11 +0300

    Created the Huffman table.

--- src/com/shpp/dbondarenko/Archiver.java
+++ src/com/shpp/dbondarenko/Archiver.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  * File: com.shpp.dbondarenko.Archiver.java
@@ -22,38 +23,65 @@ public class Archiver {
         for (HafmannTreeNode leaf : treeLeaves) {
             System.out.println(leaf);
         }
+        // System.out.println("treeLeaves: " + treeLeaves.size());
         HafmannTreeNode hafmannTreeRoot = buildHuffmanTree(treeLeaves);
-        /*HashMap<Byte, Integer> bytesFrequencyOfFile = countBytesFrequency(bytesFromFile);
-        HafmannTreeNode hafmannTreeNode = buildHuffmanTree(bytesFrequencyOfFile);
-        int count = 0;
-        for (Map.Entry<Byte, Integer> entry : bytesFrequencyOfFile.entrySet()) {
+        // System.out.println("treeLeaves: " + treeLeaves.size());
+        //  System.out.println("hafmannTreeRoot: " + hafmannTreeRoot);
+        HashMap<Byte, String> hafmannTable = createHafmannTable(treeLeaves, hafmannTreeRoot);
+        for (Map.Entry entry : hafmannTable.entrySet()) {
             System.out.println(entry.getKey() + ": " + entry.getValue());
-            count += entry.getValue();
         }
-       *//* System.out.println(bytesFrequencyOfFile.size());
-        System.out.println(count);*//*
-        HashMap<Byte, Integer> sortedBytesFrequency = sortByValue(bytesFrequencyOfFile);
-        for (Map.Entry<Byte, Integer> entry : sortedBytesFrequency.entrySet()) {
-            System.out.println(entry.getKey() + ": " + entry.getValue());
-        }*/
+    }
+
+    private HashMap<Byte, String> createHafmannTable(ArrayList<HafmannTreeNode> treeLeaves, HafmannTreeNode hafmannTreeRoot) {
+        HashMap<Byte, String> hafmannTable = new HashMap<>();
+        System.out.println("treeLeaves: " + treeLeaves.size());
+        /*byte oneByte = treeLeaves.get(0).getBytes().get(treeLeaves.get(0).getBytes().size()-1);
+        System.out.println(oneByte);
+        String idByte = createIdByte(oneByte, hafmannTreeRoot);
+        System.out.println(idByte);*/
+        for (HafmannTreeNode leaf : treeLeaves) {
+            byte oneByte = leaf.getBytes().get(0);
+            String idByte = createIdByte(oneByte, hafmannTreeRoot);
+            hafmannTable.put(oneByte, idByte);
+        }
+        System.out.println("hafmannTable: " + hafmannTable.size());
+        // System.out.println("treeLeaves: " + treeLeaves.size());
+        return hafmannTable;
+    }
+
+    private String createIdByte(Byte oneByte, HafmannTreeNode hafmannTreeRoot) {
+        String idByte = "";
+        if (hafmannTreeRoot != null && hafmannTreeRoot.getBytes().contains(oneByte)) {
+            if (hafmannTreeRoot.getLeftChild() != null &&
+                    hafmannTreeRoot.getLeftChild().getBytes().contains(oneByte)) {
+                idByte = createIdByte(oneByte, hafmannTreeRoot.getLeftChild()) + "0";
+            }
+            if (hafmannTreeRoot.getRightChild() != null &&
+                    hafmannTreeRoot.getRightChild().getBytes().contains(oneByte)) {
+                idByte = createIdByte(oneByte, hafmannTreeRoot.getRightChild()) + "1";
+            }
+        }
+        return idByte;
     }
 
     private HafmannTreeNode buildHuffmanTree(ArrayList<HafmannTreeNode> treeLeaves) {
-        System.out.println("size " + treeLeaves.size());
-        while (treeLeaves.size() > 1) {
+        ArrayList<HafmannTreeNode> treeNodes = new ArrayList<>(treeLeaves);
+        // System.out.println("size " + treeNodes.size());
+        while (treeNodes.size() > 1) {
             ArrayList<Byte> bytes = new ArrayList<>();
-            bytes.addAll(treeLeaves.get(0).getBytes());
-            bytes.addAll(treeLeaves.get(1).getBytes());
-            int frequency = treeLeaves.get(0).getFrequency() + treeLeaves.get(1).getFrequency();
-            treeLeaves.add(new HafmannTreeNode(bytes, frequency, treeLeaves.get(0), treeLeaves.get(1)));
-            System.out.println(new HafmannTreeNode(bytes, frequency, treeLeaves.get(0), treeLeaves.get(1)));
-            System.out.println("size before: " + treeLeaves.size());
-            treeLeaves.remove(0);
-            treeLeaves.remove(0);
-            Collections.sort(treeLeaves);
-            System.out.println("size after: " + treeLeaves.size());
+            bytes.addAll(treeNodes.get(0).getBytes());
+            bytes.addAll(treeNodes.get(1).getBytes());
+            int frequency = treeNodes.get(0).getFrequency() + treeNodes.get(1).getFrequency();
+            treeNodes.add(new HafmannTreeNode(bytes, frequency, treeNodes.get(0), treeNodes.get(1)));
+            //System.out.println(new HafmannTreeNode(bytes, frequency, treeNodes.get(0), treeNodes.get(1)));
+            // System.out.println("size before: " + treeNodes.size());
+            treeNodes.remove(0);
+            treeNodes.remove(0);
+            Collections.sort(treeNodes);
+            // System.out.println("size after: " + treeNodes.size());
         }
-        return null;
+        return treeNodes.get(0);
     }
 
     private ArrayList<HafmannTreeNode> createHafmannTreeLeaves(byte[] bytesFromFile) {
