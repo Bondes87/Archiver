2d0fe73e9c07b4c9fdc534bf3d78013f56d54453
Dmitro Bondarenko <bondes87@gmail.com>
2017-06-13 21:19:50 +0300

    Created threads.

--- src/com/shpp/dbondarenko/FileToArchive.java
+++ src/com/shpp/dbondarenko/FileToArchive.java
@@ -1,8 +1,6 @@
 package com.shpp.dbondarenko;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
@@ -13,23 +11,173 @@ import java.util.Map;
  * Created by Dmitro Bondarenko on 02.06.2017.
  */
 public class FileToArchive {
-
     private static final String FILE_EXTENSION = "-bds";
+    private HashMap<Byte, String> codingTable;
 
     public void createArchive(String fileName) {
-        byte[] bytesFromFile = readFileToBytes(fileName);
-        System.out.println(bytesFromFile.length);
-        HashMap<Byte, String> codingTable = createCodingTable(bytesFromFile);
-        byte[] bytesToFile = archiveFile(bytesFromFile, codingTable);
-        writeBytesToFile(bytesToFile, fileName);
+        try {
+            final PipedOutputStream output = new PipedOutputStream();
+            final PipedInputStream input = new PipedInputStream(output);
+
+            Thread thread1 = new Thread(new Runnable() {
+                private FileInputStream fileInputStream;
+
+                @Override
+                public void run() {
+                    try {
+                        fileInputStream = new FileInputStream(fileName);
+                        byte b[] = new byte[1024];
+                        int data = fileInputStream.read(b);
+                        while (data != -1) {
+                            output.write(b);
+                            data = fileInputStream.read(b);
+                        }
+                        System.out.println("output = finish");
+                        fileInputStream.close();
+                        output.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+            Thread thread2 = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        HashMap<Byte, HafmannTreeNode> treeLeavesMap = new HashMap<>();
+
+
+                        byte b[] = new byte[1024];
+                        int data = input.read(b);
+                        while (data != -1) {
+                            for (byte oneByte : b) {
+                                if (treeLeavesMap.containsKey(oneByte)) {
+                                    treeLeavesMap.get(oneByte).setFrequency(treeLeavesMap.get
+                                            (oneByte).getFrequency() + 1);
+                                } else {
+                                    ArrayList<Byte> bytes = new ArrayList<>();
+                                    bytes.add(oneByte);
+                                    treeLeavesMap.put(oneByte, new HafmannTreeNode(bytes, 1, null, null));
+                                }
+                            }
+
+                            data = input.read(b);
+                        }
+                        input.close();
+                        System.out.println("input = finish");
+
+
+                        ArrayList<HafmannTreeNode> treeLeaves = new ArrayList<>(treeLeavesMap.values());
+                        Collections.sort(treeLeaves);
+       /* for (HafmannTreeNode leaf : treeLeaves) {
+            System.out.println(leaf);
+        }*/
+                        // System.out.println("treeLeaves: " + treeLeaves.size());
+                        HafmannTreeNode hafmannTreeRoot = buildHuffmanTree(treeLeaves);
+                        // System.out.println("treeLeaves: " + treeLeaves.size());
+                        //  System.out.println("hafmannTreeRoot: " + hafmannTreeRoot);
+                        codingTable = createHafmannTable(treeLeaves,
+                                hafmannTreeRoot);
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            });
+            thread1.start();
+            thread2.start();
+            System.out.println("main wait");
+            thread2.join();
+            System.out.println("main restore");
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("main finish");
+        try {
+            final PipedOutputStream output = new PipedOutputStream();
+            final PipedInputStream input = new PipedInputStream(output);
+
+            Thread thread1 = new Thread(new Runnable() {
+                private FileInputStream fileInputStream;
+
+                @Override
+                public void run() {
+                    try {
+                        StringBuilder bitSequence = new StringBuilder();
+                        fileInputStream = new FileInputStream(fileName);
+                        byte bytes[] = new byte[1024];
+                        int data = fileInputStream.read(bytes);
+                        while (data != -1) {
+                            bitSequence.setLength(0);
+
+                            for (byte b : bytes) {
+                                bitSequence.append(codingTable.get(b));
+                            }
+
+                            //output.write(String.valueOf(bitSequence).getBytes());
+                            output.write(getBytes(bitSequence));
+                            //output.write(bytes);
+                            data = fileInputStream.read(bytes);
+                        }
+                        System.out.println("archiving finish");
+                        fileInputStream.close();
+                        output.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+
+            Thread thread2 = new Thread(new Runnable() {
+
+                @Override
+                public void run() {
+                    try {
+                        File file = new File(createFileName(fileName));
+                        file.createNewFile();
+                        FileOutputStream outputStream;
+                        outputStream = new FileOutputStream(file, true);
+
+                        byte bytes[] = new byte[1024];
+                        int data = input.read(bytes);
+                        while (data != -1) {
+                            outputStream.write(data);
+                            data = input.read(bytes);
+                        }
+                        outputStream.close();
+                        input.close();
+                        System.out.println("file write finish");
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            });
+            thread1.start();
+            thread2.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        //byte[] bytesToFile = archiveFile(bytesFromFile, codingTable);
+        //writeBytesToFile(bytesToFile, fileName);
+       /* for (Map.Entry entry : codingTable.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }*/
+        // byte[] bytesToFile = archiveFile(bytesFromFile, codingTable);
+        //writeBytesToFile(bytesToFile, fileName);
        /* for (Map.Entry entry : codingTable.entrySet()) {
             System.out.println(entry.getKey() + ": " + entry.getValue());
         }*/
     }
 
     private void writeBytesToFile(byte[] bytesToFile, String fileName) {
-        System.out.println("bytesToFile: " + bytesToFile.length);
-        /*for (byte b : bytesToFile) {
+       /* System.out.println("bytesToFile: " + bytesToFile.length);
+        for (byte b : bytesToFile) {
             System.out.println(b);
         }*/
         String name = createFileName(fileName);
@@ -58,10 +206,10 @@ public class FileToArchive {
         StringBuilder bitSequence = createBitsLine(bytesFromFile, codingTable);
         //System.out.println("bitSequence: " + bitSequence);
         // System.out.println("bitSequence length: " + bitSequence.length());
-        return getBytes(bitSequence, codingTable);
+        return getBytes(bitSequence);
     }
 
-    private byte[] getBytes(StringBuilder bitSequence, HashMap<Byte, String> codingTable) {
+    private byte[] getBytes(StringBuilder bitSequence) {
         ArrayList<Byte> bytesList = new ArrayList<>();
         byte firstByte;
         byte secondByte;
@@ -91,7 +239,8 @@ public class FileToArchive {
             }
 
         }
-        StringBuilder countBiteOfTable = new StringBuilder(Integer.toBinaryString((codingTable.size() - 1) * 3));
+        StringBuilder countBiteOfTable = new StringBuilder(Integer.toBinaryString
+                ((codingTable.size() - 1) * 3));
         // System.out.println("countBiteOfTable: " + countBiteOfTable);
         while (countBiteOfTable.length() < 16) {
             countBiteOfTable.insert(0, "0");
@@ -122,7 +271,8 @@ public class FileToArchive {
         return bytes;
     }
 
-    private StringBuilder createBitsLine(byte[] bytesFromFile, HashMap<Byte, String> codingTable) {
+    private StringBuilder createBitsLine(byte[] bytesFromFile, HashMap<Byte, String>
+            codingTable) {
         StringBuilder bitSequence = new StringBuilder();
         StringBuilder missingNumberOfBits = new StringBuilder();
         for (byte oneByte : bytesFromFile) {
@@ -151,7 +301,8 @@ public class FileToArchive {
         HafmannTreeNode hafmannTreeRoot = buildHuffmanTree(treeLeaves);
         // System.out.println("treeLeaves: " + treeLeaves.size());
         //  System.out.println("hafmannTreeRoot: " + hafmannTreeRoot);
-        HashMap<Byte, String> hafmannTable = createHafmannTable(treeLeaves, hafmannTreeRoot);
+        HashMap<Byte, String> hafmannTable = createHafmannTable(treeLeaves,
+                hafmannTreeRoot);
        /* for (Map.Entry entry : hafmannTable.entrySet()) {
             System.out.println(entry.getKey() + ": " + entry.getValue());
         }*/
@@ -159,10 +310,12 @@ public class FileToArchive {
     }
 
 
-    private HashMap<Byte, String> createHafmannTable(ArrayList<HafmannTreeNode> treeLeaves, HafmannTreeNode hafmannTreeRoot) {
+    private HashMap<Byte, String> createHafmannTable(ArrayList<HafmannTreeNode> treeLeaves,
+                                                     HafmannTreeNode hafmannTreeRoot) {
         HashMap<Byte, String> hafmannTable = new HashMap<>();
         //System.out.println("treeLeaves: " + treeLeaves.size());
-        /*byte oneByte = treeLeaves.get(0).getBytes().get(treeLeaves.get(0).getBytes().size()-1);
+        /*byte oneByte = treeLeaves.get(0).getBytes().get(treeLeaves.get(0).getBytes
+().size()-1);
         System.out.println(oneByte);
         String idByte = createIdByte(oneByte, hafmannTreeRoot);
         System.out.println(idByte);*/
@@ -205,9 +358,11 @@ public class FileToArchive {
             }
             bytes.addAll(treeNodes.get(0).getBytes());
             bytes.addAll(treeNodes.get(1).getBytes());
-            int frequency = treeNodes.get(0).getFrequency() + treeNodes.get(1).getFrequency();
+            int frequency = treeNodes.get(0).getFrequency() + treeNodes.get
+                    (1).getFrequency();
             treeNodes.add(new HafmannTreeNode(bytes, frequency, leftChild, rightChild));
-            //System.out.println(new HafmannTreeNode(bytes, frequency, treeNodes.get(0), treeNodes.get(1)));
+           /* System.out.println(new HafmannTreeNode(bytes, frequency, treeNodes.get(0),
+            treeNodes.get(1)));*/
             // System.out.println("size before: " + treeNodes.size());
             treeNodes.remove(0);
             treeNodes.remove(0);
