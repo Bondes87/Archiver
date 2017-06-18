f4d501cb3279e5b47385a886340df153a10ec0c3
Dmitro Bondarenko <bondes87@gmail.com>
2017-06-18 23:50:39 +0300

    Added file recovery from the Zip archive.

--- src/com/shpp/dbondarenko/ZipArchive.java
+++ src/com/shpp/dbondarenko/ZipArchive.java
@@ -3,6 +3,7 @@ package com.shpp.dbondarenko;
 import java.io.*;
 import java.util.Arrays;
 import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
 
 /**
@@ -11,7 +12,7 @@ import java.util.zip.ZipOutputStream;
  */
 public class ZipArchive {
     ZipArchive() {
-        fileToZip("01.MOV");
+        zipToFile("01.MOV.zip");
     }
 
     public static void main(String[] args) {
@@ -22,7 +23,7 @@ public class ZipArchive {
         try {
             final PipedOutputStream pipedOutputStream = new PipedOutputStream();
             final PipedInputStream pipedInputStream = new PipedInputStream(pipedOutputStream);
-            Thread ReaderThread = new Thread(new Runnable() {
+            Thread readerThread = new Thread(new Runnable() {
                 @Override
                 public void run() {
                     FileInputStream fileInputStream;
@@ -44,7 +45,7 @@ public class ZipArchive {
                     }
                 }
             });
-            Thread ZipWriterThread = new Thread(new Runnable() {
+            Thread zipWriterThread = new Thread(new Runnable() {
                 @Override
                 public void run() {
                     ZipOutputStream zipOutputStream;
@@ -66,8 +67,76 @@ public class ZipArchive {
                     }
                 }
             });
-            ReaderThread.start();
-            ZipWriterThread.start();
+            readerThread.start();
+            zipWriterThread.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void zipToFile(String zipName) {
+        try {
+            String[] fileName = new String[1];
+            final PipedOutputStream pipedOutputStream = new PipedOutputStream();
+            final PipedInputStream pipedInputStream = new PipedInputStream(pipedOutputStream);
+            Thread zipReaderThread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    ZipInputStream zipInputStream;
+                    try {
+                        System.out.println("Please wait.");
+                        zipInputStream = new ZipInputStream(new FileInputStream(zipName));
+                        ZipEntry entry;
+                        if ((entry = zipInputStream.getNextEntry()) != null) {
+                            fileName[0] = entry.getName();
+                            String[] nameAndExtension = fileName[0].split("\\.");
+                            fileName[0] = nameAndExtension[0] + "_copy." + nameAndExtension[1];
+                            byte[] buffer = new byte[1024];
+                            int bufferSize = zipInputStream.read(buffer);
+                            while (bufferSize != -1) {
+                                byte[] bytesToRead = Arrays.copyOfRange(buffer, 0, bufferSize);
+                                pipedOutputStream.write(bytesToRead);
+                                bufferSize = zipInputStream.read(buffer);
+                            }
+                        }
+                        zipInputStream.close();
+                        pipedOutputStream.close();
+                    } catch (IOException e) {
+                        System.out.println("Error read");
+                        e.printStackTrace();
+                    }
+                }
+            });
+
+            Thread writerThread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    FileOutputStream fileOutputStream;
+                    try {
+                        File archive = new File(fileName[0]);
+                        if (archive.exists()) {
+                            archive.delete();
+                        }
+                        archive.createNewFile();
+                        fileOutputStream = new FileOutputStream(archive, true);
+                        byte[] buffer = new byte[1024];
+                        int bufferSize = pipedInputStream.read(buffer);
+                        while (bufferSize != -1) {
+                            byte[] bytesToWrite = Arrays.copyOfRange(buffer, 0, bufferSize);
+                            fileOutputStream.write(bytesToWrite);
+                            bufferSize = pipedInputStream.read(buffer);
+                        }
+                        fileOutputStream.close();
+                        pipedInputStream.close();
+                        System.out.println("Ok" + fileName);
+                    } catch (IOException e) {
+                        System.out.println("Error write");
+                        e.printStackTrace();
+                    }
+                }
+            });
+            zipReaderThread.start();
+            writerThread.start();
         } catch (IOException e) {
             e.printStackTrace();
         }
